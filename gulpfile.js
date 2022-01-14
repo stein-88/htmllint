4f17bd905bd12128338ba3dc2e608e5a3c83396b
stein-88 <wanderson.code@icloud.com>
2022-01-14 13:20:13 -0300

    html syntax checker

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp')
+const htmllint = require('gulp-htmllint')
+const fancyLog = require('fancy-log')
+const colors = require('ansi-colors')
+
+const htmllintReporter = (filepath, issues) => {
+    if (issues.length > 0) {
+        issues.forEach((issue) => {
+            fancyLog(colors.cyan('[gulp-htmllint] ') + colors.white(filepath + ' [' + issue.line + ',' + issue.column + ']: ') + colors.red('(' + issue.code + ') ' + issue.msg))
+        })
+        process.exitCode = 1
+    }
+}
+
+gulp.task('default', () => {
+    return gulp.src('./index.html').pipe(htmllint({}, htmllintReporter))
+})
